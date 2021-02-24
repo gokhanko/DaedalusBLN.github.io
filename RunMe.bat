a5ca35c21e2014e9cc3fd6fd2a73d72f18f66f48
DaedalusBLN <>
2021-02-24 07:51:05 +0200

    Update travis...

--- RunMe.bat
+++ RunMe.bat
@@ -1,5 +1,7 @@
 @echo off
 python .\_repo_xml_generator.py
-python .\leia\_repo_xml_generator.py
-python .\matrix\_repo_xml_generator.py
+cd leia
+python .\_repo_xml_generator.py
+cd matrix
+python .\_repo_xml_generator.py
 pause
